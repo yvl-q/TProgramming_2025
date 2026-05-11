99b0d9699bc87a9144b02944cbece5a968a6db8a
Коршунов Егор Михайлович <korshunov.egor2015@ya.ru>
2026-05-11 20:55:35 +0300

    Задание 5 В6

--- main.go
+++ main.go
@@ -1,6 +1,51 @@
 package main
+
 import "fmt"
 
+// Структура Phone
+type Phone struct {
+	brand        string
+	model        string
+	calledNumber string
+}
+
+// Конструктор
+func NewPhone(brand string, model string, calledNumber string) Phone {
+	return Phone{
+		brand:        brand,
+		model:        model,
+		calledNumber: calledNumber,
+	}
+}
+
+// Метод 1: получить номер
+func (p Phone) GetCalledNumber() string {
+	return p.calledNumber
+}
+
+// Метод 2: установить номер
+func (p *Phone) SetCalledNumber(number string) {
+	p.calledNumber = number
+}
+
+// Метод 3: вывести информацию
+func (p Phone) PrintInfo() {
+	fmt.Println("Бренд:", p.brand)
+	fmt.Println("Модель:", p.model)
+	fmt.Println("Номер вызываемого абонента:", p.calledNumber)
+}
+
 func main() {
-    fmt.Println("Коршунов Егор Михайлович")
+	phone := NewPhone("Iphone", "17", "+7987654321")
+
+	fmt.Println("Информация о телефоне:")
+	phone.PrintInfo()
+
+	fmt.Println()
+
+	fmt.Println("Текущий номер:", phone.GetCalledNumber())
+
+	phone.SetCalledNumber("+7123456789")
+
+	fmt.Println("Новый номер:", phone.GetCalledNumber())
 }
